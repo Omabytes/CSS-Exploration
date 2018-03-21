809f01d782daa111a7f7507676223e3e0f36d70b
omabytes <321.trebor@gmail.com>
2018-03-21 17:06:52 +0000

    clicking on books can do stuff

--- Clip-Path/main.js
+++ Clip-Path/main.js
@@ -2,8 +2,11 @@ const books = document.querySelectorAll(".books")
 
 function expand(e) {
     console.log(this)
-    if(this.classList.contains(".expanded")) return
-    this.classList.add(".expanded")
+    if(this.classList.contains("expanded")) {
+        this.classList.remove("expanded")
+        return
+    }
+    this.classList.add("expanded")
 }
 
 books.forEach(book => book.addEventListener("click", expand))
